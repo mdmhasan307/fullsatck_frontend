9c9d9ef840a5ca316694582ccd98fa16038977ee
MD HASAN <hasan.mmd28@gmail.com>
2021-07-18 13:07:17 -0400

    basic frontend done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
+//import { HttpErrorResponse } from '@angular/common/http';
+//import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+//  title = 'employeemanagerapp';
+ // title = 'employeemanagerapp';
+   //  title = 'employeemanagerapp';
   title = 'employeemanagerapp';
+  public employees!: Employee[];
+ // public editEmployee!: Employee;
+
+ constructor(private employeeService: EmployeeService){}
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+ public getEmployees(): void{
+   this.employeeService.getEmployees().subscribe(
+     (response: Employee[])=> {
+       this.employees=response;
+     }
+        ,
+          (error: string)=>{
+          alert(error.charAt);
+       }
+   );
+ }
+
+
 }
